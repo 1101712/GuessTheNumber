c44a3c5ccfd5061d7c895da0c28834aa450c45ab
1101712 <>
2023-07-09 22:21:31 +0000

    Implemented basic structure for Number Guessing Game

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
 # Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+class NumberGuessingGame:
+    def __init__(self):
+        self.target = random.randint(1, 100)  # Target number to guess
+        self.attempts = 0  # Number of attempts made by player
+
+    def make_guess(self, guess):
+        """
+        Function to process a player's guess. If the guess is correct, the game is over. 
+        If the guess is incorrect, provides a hint to the player and increments attempts.
+        """
+        self.attempts += 1
+        if guess == self.target:
+            return "correct"
+        elif guess < self.target:
+            return "too low"
+        else:  # guess > self.target
+            return "too high"
