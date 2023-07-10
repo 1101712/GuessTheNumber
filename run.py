bb62e4e445a0f4fae8e0187cd143a225179ec130
1101712 <>
2023-07-10 19:07:45 +0000

    rework the number guessing game.  Introduce the GuessTheNumberGame class to replace the old Number Guessing Game class. Implement the check_guess method to handle guess validation and feedback within the class.The range for the random number is now adjustable by setting the low_limit and hign_limit parameters in the class constructor. Add a play method to manage the main game loop within the class. Add excepton handling for all exceptions in the game loop, not just ValueError. The total number of guesses is now stored in a list called guesses.

--- run.py
+++ run.py
@@ -1,49 +1,58 @@
 import random
 
+class GuessTheNumberGame:
+    def __init__(self, low_limit=1, high_limit=10):
+        # Set the lower and upper limits for the random number
+        self.low_limit = low_limit
+        self.high_limit = high_limit
+        
+        # Generate a random number within the specified range
+        self.target = random.randint(self.low_limit, self.high_limit)
+        
+        # Initialize an empty list to store successful guesses
+        self.guesses = []
 
-class NumberGuessingGame:
-    def __init__(self):
-        self.target = random.randint(1, 100)  # Target number to guess
-        self.attempts = 0  # Number of attempts made by player
-
-    def make_guess(self, guess):
-        """
-        Function to process a player's guess. If the guess is correct, the game is over. 
-        If the guess is incorrect, provides a hint to the player and increments attempts.
-        """
-        self.attempts += 1
-        if guess == self.target:
-            return "correct"
+    def check_guess(self, guess):
+        # Validate the guess: it must be within the specified range
+        if guess < self.low_limit or guess > self.high_limit:
+            return "Your guess is out of bounds. Try again."
+        # If the guess is less than the target, it's too low
         elif guess < self.target:
-            return "too low"
-        else:  # guess > self.target
-            return "too high"
-
-    def is_over(self):
-        """
-        Function to check if the game is over. The game is over when the correct number is guessed.
-        """
-        return self.attempts > 0 and self.make_guess(self.target) == "correct"
-
-
-game = NumberGuessingGame()
-
-while not game.is_over():
-    try:
-        # Asking user for a number
-        user_input = input("Guess a number between 1 and 100: ")
-        guess = int(user_input)
-        if guess < 1 or guess > 100:
-            # Raising a ValueError if the number is not in the expected range
-            raise ValueError("Number must be in the range 1 to 100.")
-
-        # Checking if the guess is correct, too high, or too low
-        result = game.make_guess(guess)
-        if result == "correct":
-            print(f"Congratulations! You found the number in {game.attempts} attempts.")
+            return "Too low! Try again."
+        # If the guess is more than the target, it's too high
+        elif guess > self.target:
+            return "Too high! Try again."
+        # If none of the above conditions are met, the guess is correct
         else:
-            print(f"Your guess is {result}. Try again.")
+            # Append the successful guess to the guesses list
+            self.guesses.append(guess)
+            return "Congratulations! You've guessed the number!"
 
-    except ValueError as e:
-        # Handling invalid input from the user
-        print(f"Invalid input: {e}")
+    def play(self):
+        # Start an infinite game loop
+        while True:
+            try:
+                # Ask the user to guess a number within the specified range
+                guess = int(input(f"Guess a number between {self.low_limit} and {self.high_limit}: "))
+                
+                # Check the user's guess and get the result
+                result = self.check_guess(guess)
+                print(result)
+                
+                # If the guess is correct, end the game loop
+                if result == "Congratulations! You've guessed the number!":
+                    break
+            # Handle the case where the user's input can't be converted to an integer
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+            # Handle any other exceptions that occur during the game
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
+            finally:
+                # Print the number of successful guesses made so far
+                print(f"You have made {len(self.guesses)} guesses.\n")
+
+# If this script is run directly (not imported as a module), start the game
+if __name__ == "__main__":
+    game = GuessTheNumberGame()
+    game.play()
