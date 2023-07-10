5525cd16f04da301ec9d9aaf0c723acf8762c9eb
1101712 <>
2023-07-10 09:18:24 +0000

    Add game over condition and user input handling to Number Guessing Game

--- run.py
+++ run.py
@@ -20,3 +20,21 @@ class NumberGuessingGame:
             return "too low"
         else:  # guess > self.target
             return "too high"
+
+    def is_over(self):
+        """
+        Function to check if the game is over. The game is over when the correct number is guessed.
+        """
+        return self.attempts > 0 and self.make_guess(self.target) == "correct"
+
+
+game = NumberGuessingGame()
+
+while not game.is_over():
+    try:
+        # Asking user for a number
+        user_input = input("Guess a number between 1 and 100: ")
+        guess = int(user_input)
+        if guess < 1 or guess > 100:
+            # Raising a ValueError if the number is not in the expected range
+            raise ValueError("Number must be in the range 1 to 100.")
