cf6783d262b91177f00d37a23be8d5d04f033688
1101712 <>
2023-07-21 14:37:04 +0000

    edit difficalty levels ranges in update_leaderboard and select_difficulty, fixed misspellings after PEP8 editing in program's replies

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 """
-Defines the main() function which starts the game, along with the Game class
-which is responsible for controlling the flow of the game.
+Defines the main() function which starts the game, along with
+the Game class which is responsible for controlling the flow of the game.
 """
 from pyfiglet import Figlet
 import random
 import json
 import shutil
 from colorama import Fore, Style
+
 
 class GuessTheNumberGame:
     """
@@ -15,17 +16,21 @@ class GuessTheNumberGame:
     def play_again(self):
         # A while loop to continually ask the user if they want to play again
         while True:
-            play_again = input("Would you like to play again? (yes/no): ")
-            if play_again.lower() in ["yes", "no"]:
-                if play_again.lower() == "yes":
+            play_again = input("Would you like to play again? (y/n): ")
+            if play_again.lower() in ["y", "n"]:
+                if play_again.lower() == "y":
                     # This will print a blank line for better readability.
                     print()
                     # Call the select_difficulty method to start a new game
                     self.select_difficulty()
-                # Return a boolean indicating whether the user wants to play again      
-                return play_again.lower() == "yes"
+                # Return a boolean indicating whether the user wants
+                # to play again
+                return play_again.lower() == "y"
             else:
-                print(f"{Fore.BLUE}Invalid input, please answer with 'yes' or 'no'.{Style.RESET_ALL}\n")
+                print(
+                    f"{Fore.BLUE}Invalid input, please answer with"
+                    f"'y' or 'n'.{Style.RESET_ALL}\n"
+                    )
 
     def select_difficulty(self):
         valid = False
@@ -42,14 +47,16 @@ class GuessTheNumberGame:
                     self.end = 20
                 if difficulty == "2":
                     self.start = 1
-                    self.end = 50
+                    self.end = 100
                 if difficulty == "3":
-                    self.start = -50
-                    self.end = 50
+                    self.start = -1000
+                    self.end = -1
                 # Set valid to True to exit the while loop
                 valid = True
             else:
-                print(f"{Fore.BLUE}Invalid input, please select one of the options.{Style.RESET_ALL}\n")
+                print(
+                    f"{Fore.BLUE}Invalid input, please select one"
+                    f" of the options.{Style.RESET_ALL}\n")
             # Generate a random target number for the player to guess
         self.target = random.randint(self.start, self.end)
         # Reset the list of guesses
@@ -59,10 +66,11 @@ class GuessTheNumberGame:
     def __init__(self):
         """
         Initialize the GuessTheNumberGame class.
-        The `leaderboard_file` attribute is set to the filename where the 
-        leaderboard data is stored.
-        The `select_difficulty` method is called to let the player select the 
-        difficulty level and set the range of numbers to be used in the game.
+        The `leaderboard_file` attribute is set to the filename where
+        the leaderboard data is stored.
+        The `select_difficulty` method is called to let the player
+        select the difficulty level and set the range of numbers to
+        be used in the game.
         """
         self.leaderboard_file = "leaderboard.json"
         self.select_difficulty()
@@ -70,13 +78,21 @@ class GuessTheNumberGame:
     def check_guess(self, guess):
         # Validate the guess: it must be within the specified range
         if guess < self.start or guess > self.end:
-            return False, f"{Fore.BLUE}Your guess is out of bounds. Please try again.{Style.RESET_ALL}"
+            return (False,
+                    f"{Fore.BLUE}Your guess is out of bounds."
+                    f"Please try again.{Style.RESET_ALL}")
         elif guess < self.target:
-            return True, f"{Fore.YELLOW}Too low! Please try again.{Style.RESET_ALL}"
+            return (True,
+                    f"{Fore.YELLOW}Too low! Please try again."
+                    f"{Style.RESET_ALL}")
         elif guess > self.target:
-            return True, f"{Fore.YELLOW}Too high! Please try again.{Style.RESET_ALL}"
+            return (True,
+                    f"{Fore.YELLOW}Too high! Please try again."
+                    f"{Style.RESET_ALL}")
         else:
-            return True, f"{Fore.GREEN}Congratulations! You've guessed the number!{Style.RESET_ALL}"
+            return (True,
+                    f"{Fore.GREEN}Congratulations! You've"
+                    f" guessed the number!{Style.RESET_ALL}")
 
     def play(self):
         # Start an infinite game loop
@@ -86,24 +102,35 @@ class GuessTheNumberGame:
                     f"Guess the number I'm thinking of between "
                     f"{self.start} and {self.end}: "))
                 valid, result = self.check_guess(guess)
-                # If the guess is valid (in bounds), append it to the list of guesses
+                # If the guess is valid (in bounds), append it to the
+                # list of guesses
                 if valid:
                     self.guesses.append(guess)
                 print(result)
                 if "Congratulations! You've guessed the number!" in result:
                     # Print the total number of guesses made in this game
-                    print(f"You made {len(self.guesses)} guesses in this game.\n")
-                    # Update the leaderboard with the number of guesses
-                    self.update_leaderboard(len(self.guesses))
-                    break # exit the loop when the game ends
-            # Handle the case where the user's input can't be converted to an integer
+                    print(
+                        f"You made {len(self.guesses)} guesses"
+                        f" in this game.\n"
+                        )
+                    if "Congratulations! You've guessed the number!" in result:
+                        # Update the leaderboard with the number of guesses
+                        self.update_leaderboard(len(self.guesses))
+                        break  # exit the loop when the game ends
+            # Handle the case where the user's input can't be
+            # converted to an integer
             except ValueError:
-                print(f"{Fore.BLUE}Invalid input. Please enter a number.{Style.RESET_ALL}")
+                print(
+                    f"{Fore.BLUE}Invalid input. Please enter a number."
+                    f"{Style.RESET_ALL}")
             except Exception as e:
-                print(f"{Fore.RED}An unexpected error occurred: {e}{Style.RESET_ALL}")
+                print(
+                    f"{Fore.RED}An unexpected error occurred:"
+                    f" {e}{Style.RESET_ALL}")
             # Print the total number of guesses made so far
             print(f"You have made {len(self.guesses)} guesses.\n")
-        # After the game has ended and we're out of the loop, ask if the player wants to play again
+        # After the game has ended and we're out of the loop, ask if
+        # the player wants to play again
         if self.play_again():
             # If play_again returns True, start a new game
             self.play()
@@ -111,43 +138,50 @@ class GuessTheNumberGame:
             print("Thanks for playing!")
 
     def update_leaderboard(self, score):
-        # This method updates the leaderboard and saves it to a file.
-        # Try opening the leaderboard file and loading the contents into a dictionary
+        """
+        This method updates the leaderboard and saves it to a file.
+        Try opening the leaderboard file and loading the contents
+        into a dictionary
+        """
         try:
             with open(self.leaderboard_file, 'r') as file:
                 leaderboard = json.load(file)
-        # If file not found or content is not valid JSON,
-        # create a new leaderboard with infinite scores for each difficulty        
+        # If file not found or content is not valid JSON, create a
+        # new leaderboard with infinite scores for each difficulty
         except (FileNotFoundError, json.JSONDecodeError):
-            leaderboard = {"easy": float('inf'), "medium": float('inf'), "hard": float('inf')}
-    
-        # Identify the difficulty of the current game based on the start and end numbers
+            leaderboard = {
+                "easy": float('inf'),
+                "medium": float('inf'),
+                "hard": float('inf')
+                }
+        # Identify the difficulty of the current game based
+        # on the start and end numbers
         if self.start == 1 and self.end == 20:
             difficulty = "easy"
-        elif self.start == 1 and self.end == 50:
+        elif self.start == 1 and self.end == 100:
             difficulty = "medium"
-        elif self.start == -50 and self.end == 50:
+        elif self.start == -1000 and self.end == -1:
             difficulty = "hard"
-    
+
         # Update the score for the current difficulty in the leaderboard
         leaderboard[difficulty] = min(score, leaderboard[difficulty])
-    
+
         # Write the updated leaderboard back to the file
         with open(self.leaderboard_file, 'w') as file:
             json.dump(leaderboard, file)
-    
-        # Call the show_best_score method with the updated leaderboard and current difficulty
+        # Call the show_best_score method with the updated leaderboard
+        # and current difficulty
         self.show_best_score(leaderboard, difficulty)
 
     def show_best_score(self, leaderboard, difficulty):
         if leaderboard[difficulty] < float('inf'):
             print(
                 f"The best score so far in {difficulty} mode is "
-                f"{leaderboard[difficulty]} guesses.")   
+                f"{leaderboard[difficulty]} guesses.")
 
 
 if __name__ == "__main__":
-    # Use the Figlet library to generate ASCII art for the welcome message 
+    # Use the Figlet library to generate ASCII art for the welcome message
     f = Figlet(font='slant')
     print(f.renderText('Welcome to Guess the Number Game!'))
     # Create an instance of the GuessTheNumberGame class
